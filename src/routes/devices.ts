2e8dba2ec9e9906c8ef93e703298cc0d72fea2a3
Jules Montojo <66418803+Jaymontojo@users.noreply.github.com>
2023-06-03 06:34:24 +0900

    Express server (#3)
    * install dependencies and ts types
    
    * install nodemon
    
    * feat: implement winston logger
    
    * feat: implement server configuration function and devices routes
    
    * feat: implement app entry point
    
    * feat: implement devices routes

---
+++ src/routes/devices.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from "express";
+import axios from "axios";
+import Logger from "../utils/winston";
+import dotenv from "dotenv";
+dotenv.config();
+
+//define router
+export const devicesRouter: Router = Router();
+
+//middleware
+
+//endpoints
+devicesRouter.get("/", async (req: Request, res: Response): Promise<void> => {
+  //TO DO: get devices from DB and send to FE as an array
+});
+
+devicesRouter.post("/", async (req: Request, res: Response): Promise<void> => {
+  //TO DO: post new device to DB
+});
+
+devicesRouter.delete("/:deviceID", async (req: Request, res: Response): Promise<void> => {
+  //TO DO: delete 
+});
