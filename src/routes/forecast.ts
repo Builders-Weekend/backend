f9dccf052f9399bab8ef0f04950b1360a3e28c22
Dominic Lowes <chaiskeng@gmail.com>
2023-06-03 12:23:09 +0900

    fix: remove typo from external api call

--- src/routes/forecast.ts
+++ src/routes/forecast.ts
@@ -9,10 +9,11 @@ export const forecastRouter: Router = Router();
 forecastRouter.get("/:city", async (req: Request, res: Response): Promise<void> => {
     try {
         const { city } = req.params;
-        const response = await axios.get(`https://apihttp://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=1`);
+        const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=1`);
         res.status(200).json(response.data);
     }
     catch (error) {
         Logger.error(error);
     }
 });
+
