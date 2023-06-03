bbf53f9d2a1e8a0183fd7011293613f130fca3f2
Jay <julesmontojo1@gmail.com>
2023-06-03 19:25:03 +0900

    fix: day model

--- src/models/days.ts
+++ src/models/days.ts
@@ -2,15 +2,29 @@ import { Schema, model, connect } from 'mongoose';
 import { IDevice, deviceSchema } from './devices';
 import Logger from '../utils/winston';
 
+interface IJob {
+  device: IDevice;
+  start: Date;
+  end: Date;
+  cost: number;
+}
+
+const jobSchema = new Schema<IJob>({
+  device: { type: deviceSchema, required: true},
+  start: { type: Date, required: true},
+  end: { type: Date, required: true},
+  cost: { type: Number, required: true }
+})
+
 interface IDay {
-  queuedJobs: [ IDevice ];
+  queuedJobs: [ IJob ];
   totalGreenHours: number;
   totalNonGreenHours: number;
   totalCost: number;
 }
 
 const daySchema = new Schema<IDay>({
-  queuedJobs: { type: [deviceSchema], required: true },
+  queuedJobs: { type: [jobSchema], required: true },
   totalGreenHours: { type: Number, required: true },
   totalNonGreenHours: { type: Number, required: true },
   totalCost: { type: Number, required: true },
